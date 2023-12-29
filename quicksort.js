e01cea84ad5f546e42a2aa3d67fd1ce9a9e04919
Kasia <kajzerkatarzyna1@gmail.com>
2023-12-29 19:18:29 +0100

    Added second algorithm - quicksort.js.

---
+++ quicksort.js
@@ -0,0 +1,32 @@
+const elements = [4, 2, 6, 1, 3];
+
+const swap = (arr, first, second) => {
+    const temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+}
+
+const quicksort = (arr, low, high) => {
+    if(low < high) {
+        const pivotIndex = partition(arr, low, high);
+        quicksort(arr, low, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, high)
+    }
+}
+
+const partition = (arr, low, high) => {
+    const pivotValue = arr[high];
+    let j = low;
+
+    for (let i = low; i < high; i++) {
+        if(arr[i] < pivotValue) {
+            swap(arr, j, i);
+            j++;
+        }
+    }
+    swap(arr, j, high);
+    return j;
+}
+
+quicksort(elements, 0, elements.length - 1);
+// console.log(elements);
